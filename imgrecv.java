72b21766d0c1aa2278fb8838dbf9bec3e73bdc19
Anurag Sharma <thisisanuragsharma@gmail.com>
2020-01-29 15:00:30 +0530

    Update imgrecv.java

--- imgrecv.java
+++ imgrecv.java
@@ -8,63 +8,63 @@ public class imgrecv {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    	char ch=' ';
-    	int p=0,a=0,r=0,g=0,b=0,k=0;
+		char ch=' ';
+		int p=0,a=0,r=0,g=0,b=0,k=0;
 		Scanner sc=new Scanner (System.in);
 		System.out.println("Enter the length of the message");
 		int l=sc.nextInt();
 		int count=0;
-	BufferedImage img=null;
-	try
-	{
-		img=ImageIO.read(new File("b.png"));
-	    //get image width and height 
-        int width = img.getWidth(); 
-        int height = img.getHeight();
-	    for (int i = 0; i < width && count < l; i++) {
-	        for (int j = 0; j < height && count < l; j++) {
-	        	//System.out.println(0);
-	        	p=img.getRGB(i,j);
-	        	String str="";
-	        	String t="";
-	        	int len=0;
-			    // get alpha 
-		         a = (p>>24) & 0xff;
-		         t=Integer.toBinaryString(a);
-		         len=t.length();
-		         str=t.charAt(len-2)+""+t.charAt(len-1)+str;
-		        //a=a&1;
-		  
-		        // get red 
-		         r = (p>>16) & 0xff;
-		         t=Integer.toBinaryString(r);
-		         len=t.length();
-		         str=t.charAt(len-2)+""+t.charAt(len-1)+str;
-		         //System.out.println(t);
-		        // get green 
-		         g = (p>>8) & 0xff; 
-		         t=Integer.toBinaryString(g);
-		         len=t.length();
-		         str=t.charAt(len-2)+""+t.charAt(len-1)+str;
-		         //System.out.println(t);
-		  
-		        // get blue 
-		         b = p & 0xff;
-		         t=Integer.toBinaryString(b);
-		         len=t.length();
-		         str=t.charAt(len-2)+""+t.charAt(len-1)+str;
-		         //System.out.println(t);
-		         
-		         k=Integer.parseInt(str,2);
-		         System.out.print((char)k);
+		BufferedImage img=null;
+		try
+		{
+			img=ImageIO.read(new File("b.png"));
+			//get image width and height 
+			int width = img.getWidth(); 
+			int height = img.getHeight();
+			for (int i = 0; i < width && count < l; i++) {
+				for (int j = 0; j < height && count < l; j++) {
+					//System.out.println(0);
+					p=img.getRGB(i,j);
+					String str="";
+					String t="";
+					int len=0;
+					// get alpha 
+					a = (p>>24) & 0xff;
+					t=Integer.toBinaryString(a);
+					len=t.length();
+					str=t.charAt(len-2)+""+t.charAt(len-1)+str;
+					//a=a&1;
 
-	        	count++;
-	        }
-	    }
-	}
-	catch(Exception e)
-	{
-		System.out.println(e);
-	}
+					// get red 
+					r = (p>>16) & 0xff;
+					t=Integer.toBinaryString(r);
+					len=t.length();
+					str=t.charAt(len-2)+""+t.charAt(len-1)+str;
+					//System.out.println(t);
+					// get green 
+					g = (p>>8) & 0xff; 
+					t=Integer.toBinaryString(g);
+					len=t.length();
+					str=t.charAt(len-2)+""+t.charAt(len-1)+str;
+					//System.out.println(t);
+
+					// get blue 
+					b = p & 0xff;
+					t=Integer.toBinaryString(b);
+					len=t.length();
+					str=t.charAt(len-2)+""+t.charAt(len-1)+str;
+					//System.out.println(t);
+
+					k=Integer.parseInt(str,2);
+					System.out.print((char)k);
+
+					count++;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 	}
 }
