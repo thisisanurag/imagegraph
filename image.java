dbf16935032a9129e02e499a3f7a9c4d832312a3
Anurag Sharma <thisisanuragsharma@gmail.com>
2020-01-29 14:58:42 +0530

    Update image.java

--- image.java
+++ image.java
@@ -11,72 +11,66 @@ public class image {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a message");
 		String str=sc.nextLine();
+		System.out.println("Message length="+str.length());
 		int count=0;
 		char ch=' ';
 		int p=0,a=0,r=0,g=0,b=0,k=0;
 		try
 		{
 			BufferedImage img=null;
+			img=ImageIO.read(new File("a.png"));
+		    //get image width and height 
+	        int width = img.getWidth(); 
+	        int height = img.getHeight();
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
 
-			img=ImageIO.read(new File("C:\\Users\\Anurag\\Desktop\\image\\y.jpg"));
-			
+					p=img.getRGB(i,j);// getting the RGB values at specified pixel
+					// get alpha 
+					a = (p>>24) & 0xff;
 
-			for (int i = 0; i < 5184; i++) {
-				for (int j = 0; j < 3456; j++) {
+					// get red 
+					r = (p>>16) & 0xff; 
+					// get green 
+					g = (p>>8) & 0xff; 
 
-					p=img.getRGB(i,j);
-					if (count<str.length()) {
-						ch=str.charAt(count);
-						System.out.println(ch);
+					// get blue 
+					b = p & 0xff;
+					if (count<str.length()) { // if a character exists to encode
+						ch=str.charAt(count);// extracting a character
 						count++;
-
-						// get alpha 
+						System.out.println(Integer.toBinaryString(ch));
+						//encoding last 2 bits of character in last two bits of a
 						k=ch & (0x3);
-						a = (p>>24) & 0xff;
-						//System.out.println(a);
-						a=a&(0xfc);
+						a=a&(0xfc); 
 						a=a|k;
-						System.out.println(a);
-						// get red 
+						System.out.println(Integer.toBinaryString(a));
+						//encoding next 2 bits of char in last two bits of r and so on
 						k= ch & (0xc);
-						r = (p>>16) & 0xff; 
 						r=r&(0xfc);
+						k=k>>2;
 						r=r|k;
-
-						// get green 
+						System.out.println(Integer.toBinaryString(r));
+						 
 						k= ch &(0x30);
-						g = (p>>8) & 0xff; 
 						g=g&(0xfc);
+						k=k>>4;
 						g=g|k;
-
-						// get blue 
-						k=ch &(0xc0);
-						b = p & 0xff;
-						b=b&(0xfc);
-						b=b|k;
-
-						p = (a<<24) | (r<<16) | (g<<8) | b; 
-					}
-					else
-					{
-						// get alpha 
-						a = (p>>24) & 0xff;
-
-						// get red 
-						r = (p>>16) & 0xff; 
-						// get green 
-						g = (p>>8) & 0xff; 
-
-						// get blue 
-						b = p & 0xff;
+						System.out.println(Integer.toBinaryString(g));
 						
-						p = (a<<24) | (r<<16) | (g<<8) | b;
+						k=ch &(0xc0);
+						b=b&(0xfc);
+						k=k>>6;
+						b=b|k;
+						System.out.println(Integer.toBinaryString(b));
 					}
+					// if no char is there to encode then simply reform the previous p
+					p = (a<<24) | (r<<16) | (g<<8) | b;
 					img.setRGB(i, j, p); 
 				}
 			} 
-			File outputfile = new File("C:\\Users\\Anurag\\Desktop\\image\\b.jpg");
-			ImageIO.write(img, "jpg", outputfile);
+			File outputfile = new File("b.png");
+			ImageIO.write(img, "png", outputfile);
 		}
 		catch (Exception e)
 		{
@@ -86,4 +80,3 @@ public class image {
 		System.out.println("END of programme");
 	}
 }
-//image.java
